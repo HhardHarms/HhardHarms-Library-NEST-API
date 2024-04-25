fa73615dd120b300badf20770ff3bd12b15c2252
Godwin A. Ndem <136455877+HhardHarms@users.noreply.github.com>
2024-04-25 17:39:05 +0100

    Your commit message here

---
+++ src/book/dto/create-book.dto.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { Category } from "../schemas/book.schema";
+
+
+export class CreateBookDto {
+    readonly title: string;
+    readonly description: string;
+    readonly author: string;
+    readonly price: number;
+    readonly category: Category;
+}
---
+++ src/book/dto/update-book.dto.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { Category } from "../schemas/book.schema";
+
+
+export class UpdateBookDto {
+    readonly title: string;
+    readonly description: string;
+    readonly author: string;
+    readonly price: number;
+    readonly category: Category;
+}
